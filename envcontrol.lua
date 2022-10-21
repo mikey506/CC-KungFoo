b5b4cae2b8cc61ad6b0396ac0cf72df284516661
Michael Landry <michaellandry3@hotmail.com>
2022-10-21 15:59:32 -0300

    Create envcontrol.lua

---
+++ envcontrol.lua
@@ -0,0 +1,9 @@
+[[--
+
+Simple Script to help keep weather n environmental factors within.
+
+--]]
+
+commands.execAsync("time set day")
+commands.execAsync("weather clear")
+print('Completed: Time = Day / Weater = Clear)
