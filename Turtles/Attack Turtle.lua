94d9330346463b8c9ab8ff9d4af6a75fbfa3fefe
Michael Landry <michaellandry3@hotmail.com>
2023-10-20 06:16:23 -0300

    Update Attack Turtle.lua
    Solid Foundation

--- Turtles/Attack Turtle.lua
+++ Turtles/Attack Turtle.lua
@@ -1,219 +1,65 @@
--- Color functions
-local c = {
-    r = function(text)
-        term.setTextColor(colors.red)
-        term.write(text)
-        term.setTextColor(colors.white)
-    end,
-    g = function(text)
-        term.setTextColor(colors.green)
-        term.write(text)
-        term.setTextColor(colors.white)
-    end,
-    b = function(text)
-        term.setTextColor(colors.blue)
-        term.write(text)
-        term.setTextColor(colors.white)
-    end,
-    y = function(text)
-        term.setTextColor(colors.yellow)
-        term.write(text)
-        term.setTextColor(colors.white)
-    end,
-    bl = function(text)
-        term.setTextColor(colors.black)
-        term.write(text)
-        term.setTextColor(colors.white)
-    end,
-    wh = function(text)
-        term.setTextColor(colors.white)
-        term.write(text)
-        term.setTextColor(colors.white)
-    end
-}
- 
+local movementSpeed = 0.5  -- Default movement speed
  
 -- Function to handle attacking without turning
 local function cycleAttack()
-    -- Attack sequence for each move forward
     if turtle.attack() then
-        c.r("WRN: ")
-        c.b("Attacked ")
-        c.g("Front")
-        print()
+        print("Turtle attacked in front")
     end
     if turtle.attackUp() then
-        c.r("WRN: ")
-        c.b("Attacked ")
-        c.g("Top")
-        print()
+        print("Turtle attacked above")
     end
     if turtle.attack() then
-        c.r("WRN: ")
-        c.b("Attacked ")
-        c.g("Front")
-        print()
+        print("Turtle attacked in front again")
     end
 end
- 
-local movementSpeed = 0.5  -- Default movement speed
-local consecutiveEmptyCount = 0
-local lastMoves = {} -- Table to store the last moves made by the turtle
- 
-term.clear()
-term.setCursorPos(1, 1)
-print("--==( Turtle Function Activity )==--")
-local serverID = 211  -- Replace with your server's ID
-openRednetConnection(serverID)
- 
  
 -- Function to set the movement speed
 local function setMovementSpeed(speed)
     movementSpeed = speed
-    c.bl("movementSpeed: " .. movementSpeed)
-    print()
-end
- 
--- Function to reverse the last 4 moves
-local function reverseLast4Moves()
-    c.r("WRN: ")
-    c.g("Excess_EDGE ")
-    c.b("Reversing last 4 moves")
-    print()
- 
-    -- Reverse the last 4 moves (in reverse order)
-    for i = 1, 4 do
-        local lastMove = table.remove(lastMoves)
-        if lastMove == 3 then  -- Forward
-            turtle.back()
-        elseif lastMove == 1 then  -- Left
-            if not turtle.detect() then
-                if not turtle.turnRight() then
-                    c.r("WRN: ")
-                    c.g("Blocked while turning right during reversal")
-                    print()
-                end
-                if not turtle.forward() then
-                    c.r("WRN: ")
-                    c.g("Blocked during forward move during reversal")
-                    print()
-                end
-            else
-                c.r("WRN: ")
-                c.g("Blocked during left turn during reversal")
-                print()
-            end
-        elseif lastMove == 2 then  -- Right
-            if not turtle.detect() then
-                if not turtle.turnLeft() then
-                    c.r("WRN: ")
-                    c.g("Blocked while turning left during reversal")
-                    print()
-                end
-                if not turtle.forward() then
-                    c.r("WRN: ")
-                    c.g("Blocked during forward move during reversal")
-                    print()
-                end
-            else
-                c.r("WRN: ")
-                c.g("Blocked during right turn during reversal")
-                print()
-            end
-        end
-    end
+    print("Movement speed set to: " .. movementSpeed)
 end
  
 -- Function to move forward, left, or right with collision detection and avoidance
 local function moveRandomly()
     local direction = math.random(1, 4)  -- Randomly choose a direction (1: left, 2: right, 3-4: forward)
-    table.insert(lastMoves, direction)  -- Add the current move to the lastMoves table
+    local detectSuccess, blockData = turtle.inspectDown()
  
-    -- Attempt to move in the chosen direction
     if direction >= 3 and direction <= 4 then
-        local detectSuccess, blockData = turtle.inspectDown()
- 
         if detectSuccess and (blockData.name == "minecraft:sand" or blockData.name == "minecraft:air" or blockData.name == "minecraft:water") then
-            c.r("WRN: ")
-            c.g("Det: "..blockData.name)
-            c.wh(" - ")
-            c.wh("MV ")
-            c.y("Back2 Right2")
-            print()
+            print("Detected " .. blockData.name)
+            local whichway = math.random(1, 2)
+            print("Moving back and turning right on restricted area")
             turtle.back(2)
-            turtle.turnRight(2)
-            consecutiveEmptyCount = consecutiveEmptyCount + 1
-            if consecutiveEmptyCount >= 3 then
-                reverseLast4Moves()  -- Reverse the last 3 moves
-                return
-            end
-            return
-        else
-            consecutiveEmptyCount = 0
-            if turtle.forward() then
-                return
-            else
-                c.r("WRN: ")
-                c.b("Blocked ")
-                c.g("FORWARD_RIGHT")
-                print()
+            if whichway == 1 then
                 turtle.turnRight()
+                print("Turning Right on restricted area")
+            else
+                turtle.turnLeft()
+                print("Turning Left on restricted area")
+            end
+        else
+            if not turtle.forward() then
+                print("Turtle blocked while moving forward")
+            else
+                print("Turtle moving forward")
+                cycleAttack()
                 return
             end
         end
-    end
-    if direction == 1 then
-        if not turtle.detect() then
-            if turtle.turnLeft() then
-                cycleAttack()
-                if turtle.forward() then
---                    c.g("NOTE: ")
---                    c.b("Move  ")
---                    c.y("LEFT & FORWARD")
---                    print()
-                else
-                    c.g("NOTICE: ")
-                    c.r("Move LEFT ")
-                    c.g("LEFT")
-                    print()
-                end
-            else
-                c.r("WRN: ")
-                c.b("Locked while turning left")
-                print()
-            end
-        elseif direction == 2 then
-            if not turtle.detect() then
-                if turtle.turnRight() then
-                    if turtle.forward() then
-                        c.g("NOTE: ")
-                        c.r("Move ")
-                        c.b("RIGHT")
-                        print()
-                        return
-                    else
-                        c.g("NOTICE: ")
-                        c.r("Move ")
-                        c.b("RIGHT")
-                        print()
-                        turtle.turnRight()
-                        return
-                    end
-                else
-                    c.r("WRN: ")
-                    c.b("Blocked ")
-                    c.g("while turning right")
-                    print()
-                end
-            end
+    elseif direction == 1 then
+        if detectSuccess and (blockData.name == "minecraft:sand" or blockData.name == "minecraft:air" or blockData.name == "minecraft:water") then
+            return
         end
+        turtle.turnRight()
+        print("Turning Right")
+    elseif direction == 2 then
+        if detectSuccess and (blockData.name == "minecraft:sand" or blockData.name == "minecraft:air" or blockData.name == "minecraft:water") then
+            return
+        end
+        turtle.turnLeft()
+        print("Turning Left")
     end
 end
- 
--- Usage: Replace 'serverID' with the ID of your server
-local serverID = 122  -- Replace with your server's ID
-openRednetConnection(serverID)
- 
  
 -- Main loop
 while true do
